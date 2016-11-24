31f98e6eedc4f5a99064b8fa7363feb01933f625
Neil Weintraut <neil@Neils-iMac.local>
2016-11-24 14:16:34 -0800

    Gradient Background

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -5,6 +5,7 @@ import {
 } from 'react-native'
 import StatusScreen from './StatusScreen'
 import TabBarContainer from './TabBarContainer'
+import LinearGradient from 'react-native-linear-gradient'
 
 class MainContainer extends Component {
 	_renderTab(selected, services) {
@@ -18,13 +19,13 @@ class MainContainer extends Component {
 	}
 	render() {
 		return (
-			<View style={styles.container}>
+			<LinearGradient colors={['#313d43', '#4a787a']} style={styles.container}>
 				{this._renderTab(this.props.selectedService, this.props.services)}
 				<TabBarContainer 
 					onTabChange={this.props.onServiceChange} 
 					selectedService={this.props.selectedService}
 				/>
-			</View>
+			</LinearGradient>
 		)
 	}
 }
