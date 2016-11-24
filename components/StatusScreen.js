df3809a9365686b015c6e1019d0c89d8f0ed6340
Neil Weintraut <neil@Neils-iMac.local>
2016-11-24 12:37:54 -0800

    Working

--- components/StatusScreen.js
+++ components/StatusScreen.js
@@ -10,7 +10,7 @@ class StatusScreen extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<StatusIndicator isUp={true}/>
+				<StatusIndicator isUp={this.props.isUp}/>
 				<Text style={styles.statusText}>Service {this.props.isUp ? 'Up' : 'Down'}</Text>
 			</View>
 		)
